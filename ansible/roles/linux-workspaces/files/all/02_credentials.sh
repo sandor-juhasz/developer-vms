e8b4bfc9baa0c945dcc7b919e99386f2afd2881b
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2021-11-11 06:52:38 +0100

    Using Zenity to input credential file.

--- ansible/roles/linux-workspaces/files/all/02_credentials.sh
+++ ansible/roles/linux-workspaces/files/all/02_credentials.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-read -p "Please enter the path to your credentials TGZ." credentials
-cd ~ || exit
-tar xvzf "${credentials}"
+# TODO:
+#    - Show information dialog first, describing what a credential file is.
+#      Ability to continue or hit cancel.
+#
+credentials=$(zenity --file-selection \
+                     --title "Please select the credenitals package to install" \
+                     --file-filter="*.tgz")
+# read -p "Please enter the path to your credentials TGZ." credentials
+# Use text-based dialog commands when the command is not running on X.
+
+if [[ $? == 0 ]]; then
+   cd ~ || exit
+   tar xvzf "${credentials}"
+elif [[ $? == 1 ]]; then 
+   echo "User interruption, skipping credentials install."
+fi
